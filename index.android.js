13b9c8c5bab383b2c74cecac5326c0e28b83af16
Oskar Niburski <oskarniburski@gmail.com>
2016-02-02 22:58:59 -0500

    Android view

--- index.android.js
+++ index.android.js
@@ -1,51 +1,70 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-import React, {
+var React = require('react-native');
+var {
   AppRegistry,
-  Component,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  TextInput,
+  Image,
+  ScrollView,
+  Navigator,
+  NavigatorIOS
+} = React;
 
-class whattodoNative extends Component {
+var Dimensions = require('Dimensions');
+
+var REQUEST_URL = 'http://localhost:3000/api/v1/today';
+let things = []
+
+var EventForm             = require('./EventForm');
+var EventList             = require('./EventList');
+var MainApp            = require('./MainApp');
+
+var styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: '#85b16a'
+  }
+});
+
+
+class whattodoNative extends React.Component {
+  constructor(props) {
+    super(props);    
+    component = this;
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+        <NavigatorIOS
+        barTintColor='#000'
+        titleTextColor='#fff'
+        tintColor='#fff'
+        style={styles.container}
+        initialRoute={{
+          title: 'WhatToDo',
+          component: MainApp
+        }}/>              
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+var {width, height} = Dimensions.get('window');
+
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'white',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    alignItems: 'stretch',
+    backgroundColor: '#BA55D3',
+  }
+})
 
 AppRegistry.registerComponent('whattodoNative', () => whattodoNative);
